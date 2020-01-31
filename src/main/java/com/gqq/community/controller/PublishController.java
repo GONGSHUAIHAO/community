d0c66accc3228104b9d131995562c239a393b047
GQQQQQQ <369787331@qq.com>
2020-01-31 14:29:30 +0800

    add publish

--- src/main/java/com/gqq/community/controller/PublishController.java
+++ src/main/java/com/gqq/community/controller/PublishController.java
@@ -1,7 +1,18 @@
 package com.gqq.community.controller;
 
+import com.gqq.community.mapper.QuestionMapper;
+import com.gqq.community.mapper.UserMapper;
+import com.gqq.community.model.Question;
+import com.gqq.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @program: PublishController
@@ -13,8 +24,60 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublishController {
 
-    @GetMapping("publish")
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/publish")
     public String publish(){
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam("title") String title,
+                            @RequestParam("description") String description,
+                            @RequestParam("tag") String tag,
+                            HttpServletRequest request,
+                            Model model){
+
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+
+        if(title == null || title == ""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+
+        User user = null;
+
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie: cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
+        if(user == null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        questionMapper.insertQuestion(question);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/gqq/community/mapper/QuestionMapper.java
@@ -0,0 +1,12 @@
+package com.gqq.community.mapper;
+
+import com.gqq.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    public void insertQuestion(Question question);
+}
